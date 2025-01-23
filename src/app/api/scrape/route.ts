658c2d344fa45fef3eb5b35d6619df7b6230833a
Saymon Porras Briones <118936420+CtpN3m01@users.noreply.github.com>
2025-01-23 11:23:35 -0600

    fix: testing

--- src/app/api/scrape/route.ts
+++ src/app/api/scrape/route.ts
@@ -9,7 +9,9 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ error: 'URL is required' }, { status: 400 });
     }
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
     const page = await browser.newPage();
 
     await page.goto(url, { waitUntil: 'networkidle2' });
@@ -18,17 +20,14 @@ export async function POST(req: NextRequest) {
     const title = await page.title();
 
     // Extrae la descripción completa
-    //const description = await page.$eval('div.d1isfkwk', el => el.textContent?.trim() || '');
-    const description = ""
-    // Extrae el precio
-    //const price = await page.$eval('div._1jo4hgw', el => el.textContent?.trim() || '');
-    const price = ""
+    const description = await page.$eval('div.d1isfkwk', el => el.textContent?.trim() || '');
 
-    // Extrae las URLs de las fotos que contienen 'im/pictures/miso/Hosting-'
+    // Extrae el precio
+    const price = ''; // Actualiza este selector según tus necesidades
+
+    // Extrae las URLs de las fotos que contienen 'im/pictures/hosting'
     const photos = await page.$$eval('img', imgs =>
-        imgs
-        .map(img => img.src)
-        .filter(src => src.includes('im/pictures/'))
+      imgs.map(img => img.src).filter(src => src.includes('im/pictures/hosting'))
     );
 
     await browser.close();
